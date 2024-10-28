c71f22b33727380e304d10a58eb94b621c77e2d8
mxg-mega <68956501+mxg-mega@users.noreply.github.com>
2024-10-28 06:43:12 +0100

    Updated the api

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of axios with the base URL of the Flask API
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:5000',  // Update this if your backend runs on a different port
+  baseURL: 'https://web-production-46a8.up.railway.app',  // Update this if your backend runs on a different port
   headers: {
     'Content-Type': 'application/json'
   }
