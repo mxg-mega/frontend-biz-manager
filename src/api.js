3b585c08e7c01c76c5d9337e1b61b3c43a090f2b
mxg-mega <68956501+mxg-mega@users.noreply.github.com>
2024-10-28 09:36:41 +0100

    added an env

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of axios with the base URL of the Flask API
 const api = axios.create({
-  baseURL: 'https://web-production-46a8.up.railway.app',
+  baseURL: process.env.REACT_APP_API_URL,
   // baseURL: "http://127.0.0.1:5000",
   headers: {
     'Content-Type': 'application/json'
